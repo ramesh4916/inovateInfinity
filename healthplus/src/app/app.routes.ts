6bc667b54336d39985274d8634051558503c2c8c
ramesh4916 <ramesh.004916@gmail.com>
2024-02-20 17:21:47 +0530

    modified

--- healthplus/src/app/app.routes.ts
+++ healthplus/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { ContactComponent } from './contact/contact.component';
 import { DoctorComponent } from './doctor/doctor.component';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
+import { HelplineComponent } from './helpline/helpline.component';
 
 export const routes: Routes = [
     {'path':'','title':'Home',component:HomeComponent},
@@ -13,4 +14,5 @@ export const routes: Routes = [
     {'path':'contact','title':'Contact',component:ContactComponent},
     {'path':'signup','title':'Sign Up',component:SignupComponent},
     {'path':'login','title':'LogIn',component:LoginComponent},
+    {'path':'helpline','title':'Helpline',component:HelplineComponent},
 ];
