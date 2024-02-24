b936291e34f50ec0b4cd0dcb2cca1c6ab24f3a27
ramesh4916 <ramesh4916@gmail.com>
2024-02-24 19:10:10 +0530

    modified

--- healthplus/src/app/dr-naveen/dr-naveen.component.ts
+++ healthplus/src/app/dr-naveen/dr-naveen.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-dr-naveen',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './dr-naveen.component.html',
   styleUrl: './dr-naveen.component.css'
 })
