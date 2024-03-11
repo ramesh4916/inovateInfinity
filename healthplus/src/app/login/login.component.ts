ae5783ef49a1679680b25f27b546e57290d973ab
ramesh4916 <ramesh4916@gmail.com>
2024-03-11 12:17:49 +0530

    modified

--- healthplus/src/app/login/login.component.ts
+++ healthplus/src/app/login/login.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { validateHeaderValue } from 'http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  email =new FormControl("",[
+    Validators.required,
+    Validators.email
+  ])
+  password = new FormControl("",[
+    Validators.required,
+    Validators.minLength(6)
+  ])
 
+  loginForm = new FormGroup({
+    email:this.email,
+    password:this.password
+  })
+  login(){
+    console.log(this.loginForm.value)
+  }
 }
